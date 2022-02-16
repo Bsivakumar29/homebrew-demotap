2ceeca0c61821118062758532b4e11845a72bf04
Kevin <kevin@Kevins-MacBook-Pro.local>
2022-02-15 21:56:41 -0800

    add formulae

---
+++ demotap.rb
@@ -0,0 +1,15 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Demotap < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/Bsivakumar29/demotap/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "f556dd58fa4a54bc0f6f442d240061a934abedc74fe9ddb3c5b71c7983547d9e"
+  license ""
+
+  def install
+	bin.install "demotap"
+  end
+
+end
